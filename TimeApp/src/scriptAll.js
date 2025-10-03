0792279d5e5fef8fbe065acd5695ea5b442a8cdb
C0mradeAndThePhantomMenace <Ang3lOfDarkn355@proton.me>
2025-10-03 09:54:25 +0000

    5

--- TimeApp/src/scriptAll.js
+++ TimeApp/src/scriptAll.js
@@ -14,9 +14,46 @@ function updateClock() {
     const timeString = `${hours}:${minutes}:${seconds}`;
     document.getElementById('clock').textContent = timeString;
 }
+let timeLeft = 10; // Initial countdown time in seconds
+
+function updateCountdown() {
+    const countdownDisplay = document.getElementById('countdown');
+    countdownDisplay.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+        clearInterval(timerInterval);
+        countdownDisplay.textContent = "Time's up!";
+    }
+    timeLeft--;
+}
+
+
+let timeCounter = 0; // Initial countdown time in seconds
+
+function updateCounter() {
+    const countdownDisplay = document.getElementById('counter');
+    countdownDisplay.textContent = timeCounter;
+
+    timeCounter++;
+}
+
+// Update the countdown every second
+// const timerInterval = setInterval(updateCountdown, 1000);
 
 // Update the clock every second
-setInterval(updateClock, 1000);
+// setInterval(updateClock, 1000);
 
+function executeAllFunctions() {
+    updateClock();
+    updateCounter();
+    // updateCountdown();
+}
+
+setInterval(executeAllFunctions, 1000);
+
+// Initial call to display the counter immediately
+updateCounter();
+// Initial call to display the countdown immediately
+// updateCountdown();
 // Initial call to display the time immediately
 updateClock();
