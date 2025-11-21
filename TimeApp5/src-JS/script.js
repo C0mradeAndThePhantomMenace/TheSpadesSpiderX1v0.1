a8b015f35e515b9e5fbd8664181efa09d309cce8
C0mradeAndThePhantomMenace <Ang3lOfDarkn355@proton.me>
2025-11-21 13:22:42 +0000

    TimeApp5

--- TimeApp5/src-JS/script.js
+++ TimeApp5/src-JS/script.js
@@ -5,7 +5,7 @@ console.log(`script.js >> Have a nice day!`)
 async function load_home() {
     var content = document.getElementById("Clock");
     // content.innerHTML = await (await fetch('../src-html/Clock/Clock.html')).text().catch((error) => { console.log(error) });
-    let ht = await (await fetch('../src-html/Clock/Clock.html')).text()
+    let ht = await (await fetch('/TimeApp5/src-html/Clock/Clock.html')).text()
     console.log(ht)
     content.innerHTML= ht
 }
